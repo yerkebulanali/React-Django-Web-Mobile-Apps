ad45a5c5004145e95b79fa9e9275aee97ef86c06
Yerkebulan <yerkebulanali@gmail.com>
2022-10-02 20:50:37 +0600

    custom method

--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
 from .models import Movie, Rating
 from .serializers import MovieSerializers, RatingSerializers
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializers
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
