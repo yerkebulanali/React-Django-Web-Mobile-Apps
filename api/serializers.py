be90bb1fbf8701f0e9239dff6ecbca2c547b2d4b
Yerkebulan <yerkebulanali@gmail.com>
2022-10-02 16:04:29 +0600

    serializers

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Movie, Rating
+
+
+class MovieSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'description')
+
+
+class RatingSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'stars', 'user', 'movie')
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Rating
+from .serializers import MovieSerializers, RatingSerializers
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializers
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializers
+
